f60c029c8472d6e567a7c01658bd08628b76b823
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2023-08-04 17:15:55 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "John", 
+    streetAddress: "New York",
+};
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value
+    return newEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    const cloneEmployee = {...employee};
+    delete cloneEmployee[key];
+    return cloneEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+};
+
